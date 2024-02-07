800a65f8a0eb6783a1d982533d3d5cd9011153dd
B-Oralbayev <b_oralbayev@kbtu.kz>
2024-02-07 23:47:32 +0600

    lab3 commit

---
+++ lab3/classes/classes.py
@@ -0,0 +1,74 @@
+import math
+
+#1
+class String:
+    def __init__(self) -> None:
+        self.input_string = ""
+
+    def getString(self):
+        self.input_string = str(input())
+
+    def printString(self):
+        print(self.input_string.upper())
+
+
+#2, 3        
+class Shape:
+    def __init__(self):
+        self.area = 0
+        
+class Square:
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length * self.length
+
+class Rectangle:
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+#4
+class Point():
+    def __init__(self, x, y):
+        self.x1 = x
+        self.y1 = y
+    
+    def show(self):
+        return self.x1, self.y1
+    
+    def move(self, x, y):
+        self.x1 = x
+        self.y1 = y
+    
+    def dist(self, x, y):
+        return (int(math.sqrt((x - self.x1) **2 + (y - self.y1) ** 2)))
+
+
+#5
+class Account():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+    def deposit(self, num):
+        self.balance += num
+        print(f"Deposit of {num} successful. Current balance: {self.balance}")
+    def withdraw(self, num):
+        if num > self.balance:
+            print('Not enough money')
+        else:
+            self.balance -= num
+            print(f"Withdrawal of {num} successful. Current balance: {self.balance}")
+
+#6
+def IsPrime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
